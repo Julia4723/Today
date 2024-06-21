ce68c8135979dc986b37ac6a95ac1d5a71e8d432
My <uk472363@yanex.ru>
2024-06-21 18:49:39 +0300

    Add PinnedSubView

---
+++ Today/DetailViewController/PriorityContentView.swift
@@ -0,0 +1,8 @@
+//
+//  PriorityContentView.swift
+//  Today
+//
+//  Created by user on 21.06.2024.
+//
+
+import Foundation
---
+++ Today/DetailViewController/ReminderViewController + CellConfiguration.swift
@@ -0,0 +1,8 @@
+//
+//  ReminderViewController + CellConfiguration.swift
+//  Today
+//
+//  Created by user on 21.06.2024.
+//
+
+import Foundation
